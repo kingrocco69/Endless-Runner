705d0f0ff9d2d6ec5d9bd8a790ed08f068a774ef
rushikeshdesai10 <78646250+rushikeshdesai10@users.noreply.github.com>
2021-06-15 11:51:03 +0530

    Add files via upload

---
+++ power.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class power : MonoBehaviour
+{
+
+     private GameObject player;
+     private GameObject powerup;
+
+    public spawnObstacle incr;
+        
+
+    private float Timer=5f;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+         player =GameObject.FindGameObjectWithTag("player");
+         powerup=GameObject.FindGameObjectWithTag("power");
+
+            
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.tag=="player"){
+             StartCoroutine(fade());
+                   
+            Destroy(powerup.gameObject);
+        }
+
+    }
+     private IEnumerator fade(){
+
+            incr.timeBetweenSpawn=2;
+            
+             yield return new WaitForSeconds(5);
+             Debug.Log("hello");
+            incr.timeBetweenSpawn=1;
+                
+                
+     }
+    
+
+    
+
+    
+}
